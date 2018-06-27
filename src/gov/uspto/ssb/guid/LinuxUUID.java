fc5dfb6450e1860842012f25417c96180c05e412
Spence <spence@srspencer.com>
2018-06-27 19:51:04 -0400

    LinuxUUID Code Fix

--- src/gov/uspto/ssb/guid/LinuxUUID.java
+++ src/gov/uspto/ssb/guid/LinuxUUID.java
@@ -2,7 +2,7 @@ package gov.uspto.ssb.guid;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 
 public class LinuxUUID{
@@ -10,19 +10,30 @@ public class LinuxUUID{
      
 
         public LinuxUUID()throws IOException{
-                char[] cbuf = new char[36];
+                StringBuffer buf = new StringBuffer();
               
                 
                 FileInputStream fis;
                 InputStreamReader in;
                 
-                
+                		String str="";
                         String fileToRead = new String("/proc/sys/kernel/random/uuid");
                         fis = new FileInputStream(fileToRead);
                         in = new InputStreamReader(fis, "UTF-8");
-
+                        try {
+                            BufferedReader reader = new BufferedReader(in);
+                            if (in != null) {                            
+                                while ((str = reader.readLine()) != null) {    
+                                    buf.append(str + "\n" );
+                                }                
+                            }
+                            reader.close();
+                        } finally {
+                            try { fis.close(); } catch (Throwable ignore) {}
+                        }
                
-                        s = new String(cbuf);
+                        s = new String(buf);
+                        
                         fis.close();
                         in.close();
                        
@@ -46,3 +57,4 @@ public class LinuxUUID{
         		}
         }
 }
+
